7b862fada39ed3426fe84eff9b2df18fda58c4a7
Shesha Shankar <shesha1096@gmail.com>
2021-05-25 19:28:19 +1000

    Fixing issues with POST request

--- src/services/addTag.js
+++ src/services/addTag.js
@@ -4,17 +4,19 @@ const apiEndPoint =
   "https://dc7wcopoi1.execute-api.us-east-1.amazonaws.com/prod/tagtag/update";
 
 const token = localStorage.getItem("accessToken");
-
+console.log('Token: ' + token);
 function updateTag(url, tag) {
+  
   const params = {
     url,
     tags: tag,
   };
   const config = {
-    headers: { Authorization: `Bearer ${token}` },
-    params,
-  };
-  return axios.post(apiEndPoint, config);
+    headers: { Authorization: `Bearer ${token}`
+  }
+};
+console.log('Tags ' + typeof(tag));
+  return axios.post(apiEndPoint, params,config);
 }
 
 export { updateTag };
